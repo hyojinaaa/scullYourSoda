09b5ec39131b5fd7119498d1ef9b21af30589e2c
Hyojin Jung <hyojin@getproperly.com>
2019-02-04 14:17:52 +1300

    Specify npm version

--- constants/frontend.js
+++ constants/frontend.js
@@ -2,7 +2,4 @@ const FRONTEND_DEV_URLS = ['http://localhost:3000'];
 
 const FRONTEND_PROD_URLS = ['https://www.scullyoursoda.com'];
 
-module.exports =
-  process.env.NODE_ENV === 'production'
-    ? FRONTEND_PROD_URLS
-    : FRONTEND_DEV_URLS;
+module.exports = process.env.NODE_ENV === 'dev' ? FRONTEND_DEV_URLS : FRONTEND_PROD_URLS;
